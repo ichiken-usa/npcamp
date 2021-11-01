947c810c7106d4b347c738a743af088e1e8eac78
Ichiken <ichiken.usa@gmail.com>
2021-10-31 20:10:32 -0700

    First test

---
+++ log.py
@@ -0,0 +1,26 @@
+from logging import StreamHandler, DEBUG, INFO, Formatter
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+import os
+ 
+def set_log_config(logger, dir, filename):
+ 
+    # ---- Prepare log folder ----
+    os.makedirs(dir, exist_ok=True) 
+    filepath = dir + filename
+ 
+    # ---- handler1: For terminal ----
+    handler1 = StreamHandler()
+    handler1.setLevel(DEBUG)
+    handler1.setFormatter(Formatter("[%(asctime)s][%(levelname)8s][%(name)s] %(message)s"))
+ 
+    # ---- handler2: For log file ----
+    #handler2 = TimedRotatingFileHandler(filename = filepath, when='midnight', backupCount=30, encoding='utf-8') # If you want to rotate file by time, use this
+    handler2 = RotatingFileHandler(filename = filepath, maxBytes = 1048576, backupCount = 10, encoding='utf-8', ) # 1MB, keep 10 files
+    handler2.setLevel(INFO)
+    handler2.setFormatter(Formatter("[%(asctime)s][%(levelname)8s][%(name)s] %(message)s"))
+ 
+    # ---- Set log config ----
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    logger.propagate = False
